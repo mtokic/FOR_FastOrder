9d0a9466c54bf055fb86b47da02670331650c7e0
Karlo <kbuturac@foi.hr>
2013-05-19 22:52:52 +0200

    Upute forma
    karlo

---
+++ FOR_Application/Kreiranje narudžbe/Upute.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Kreiranje_narudžbe
+{
+    partial class frmUpute
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNazad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNazad
+            // 
+            this.btnNazad.Location = new System.Drawing.Point(408, 283);
+            this.btnNazad.Name = "btnNazad";
+            this.btnNazad.Size = new System.Drawing.Size(75, 23);
+            this.btnNazad.TabIndex = 0;
+            this.btnNazad.Text = "Nazad";
+            this.btnNazad.UseVisualStyleBackColor = true;
+            this.btnNazad.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmUpute
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 360);
+            this.Controls.Add(this.btnNazad);
+            this.Name = "frmUpute";
+            this.Text = "Upute";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNazad;
+    }
+}
