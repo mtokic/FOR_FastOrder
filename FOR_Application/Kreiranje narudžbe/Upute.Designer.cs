c327d66f98aae1cde5a539505e46236d38dba27c
antun ivancic <antun.kramp@gmail.com>
2013-06-20 22:36:32 +0200

    Forma za upute

--- FOR_Application/Kreiranje narudžbe/Upute.Designer.cs
+++ FOR_Application/Kreiranje narudžbe/Upute.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // btnNazad
             // 
-            this.btnNazad.Location = new System.Drawing.Point(412, 314);
+            this.btnNazad.Location = new System.Drawing.Point(487, 354);
             this.btnNazad.Name = "btnNazad";
-            this.btnNazad.Size = new System.Drawing.Size(75, 23);
+            this.btnNazad.Size = new System.Drawing.Size(125, 75);
             this.btnNazad.TabIndex = 0;
             this.btnNazad.Text = "Nazad";
             this.btnNazad.UseVisualStyleBackColor = true;
@@ -45,11 +45,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(499, 360);
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(624, 441);
             this.Controls.Add(this.btnNazad);
             this.Name = "frmUpute";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Upute";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
         }
