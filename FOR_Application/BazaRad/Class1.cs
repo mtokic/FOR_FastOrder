36d0a21fe2e296746c1a9fd8d59b97d3560e0486
Mario Tokić <mtokic@foi.hr>
2013-06-21 01:24:19 +0200

    klasa za bazu(jos ne gotova)

---
+++ FOR_Application/BazaRad/Class1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+
+namespace bazaRad
+{
+    /// <summary>
+    /// Kreiramo klasu za rad s bazom podataka
+    /// </summary>
+    
+    public class bazaRad
+    {
+        private SqlConnection connection;
+        private string ConnectionString = "Data Source=161.53.120.217\\VARAZDIN,1433;Initial Catalog=pi2013FastOrderdb;User ID=pi2013FastOrder;Password=pi2013FastOrder";
+
+
+        /// <summary>
+        /// Kreiramo konstruktor
+        /// </summary>
+        public bazaRad()
+        {
+        }
+
+        /// <summary>
+        /// Metoda kojom cemo otvarati nove konekcije prema bazi
+        /// </summary>
+        private bool OpenConnection()
+        {
+            connection = new SqlConnection(ConnectionString);
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (SqlException e)
+            {
+//           MessageBox.Show(e.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Metoda za izvršavanje SQL upita nad bazom
+        /// </summary>
+        /// <param name="query">Sadržaj upita</param>
+        protected bool Query(string query)
+        {
+            bool val = false;
+            if (OpenConnection())
+            {
+                SqlCommand cmd = new SqlCommand(query, connection);
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    val = true;
+                }
+                catch (SqlException e)
+                {
+  //                  MessageBox.Show(e.Message);
+                    val = false;
+                }
+            }
+            return val;
+
+        }
+
+        /// <summary>
+        /// Metoda za izvršavanje SQL upita nad bazom
+        /// </summary>
+        /// <param name="query">Upit</param>
+        /// <returns></returns>
+        public SqlDataReader QueryReader(string query)
+        {
+            OpenConnection();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            return cmd.ExecuteReader();
+        }
+
+        /// <summary>
+        /// Metoda kojom zatvaramo otvorenu konekciju
+        /// </summary>
+        protected bool CloseConnection()
+        {
+            bool val = false;
+            if (connection != null)
+            {
+                val = true;
+                connection.Close();
+                connection.Dispose();
+            }
+            return val;
+        }
+
+
+    }
+}
---
+++ FOR_Application/dbConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+/// <summary>
+/// Klasa za rad sa bazom podataka
+/// </summary>
+
+
+ class dbConfig
+{
+
+	public dbConfig()
+	{
+	}
+
+    private  
+    private string ConnectionString = "Data Source= ";
+}
