1de5a1212318271ecbe8b3c8e750a748214424da
Mario Tokić <mtokic@foi.hr>
2013-06-22 01:01:54 +0200

    noviZahtjevi

---
+++ FOR_Application/FOR_Application/noviZahtjev.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using DBHelper;
+
+namespace FOR_Application
+{
+    class noviZahtjev : bazaRad
+    {
+        string upit = "Select ID_narudzbe from Narudzba where IDstatus = 1;";
+        frmPregledNarudzbi pregled;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="Pregled"></param>
+        public noviZahtjev(frmPregledNarudzbi Pregled)
+        {
+            this.pregled = Pregled;
+        }
+
+        public void pregledajNoveZahtjeve()
+        {
+
+
+
+        }
+
+
+    }
+}
