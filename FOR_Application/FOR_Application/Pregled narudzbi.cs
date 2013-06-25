7fdafe46463ff9da264443db5d90d0456eddc86a
Mario Tokić <mtokic@foi.hr>
2013-06-25 17:25:24 +0200

    ticker

--- FOR_Application/FOR_Application/Pregled narudzbi.cs
+++ FOR_Application/FOR_Application/Pregled narudzbi.cs
@@ -15,19 +15,17 @@ namespace FOR_Application
     {
         public static ToolStripStatusLabel _status;
         private Timer timer = new Timer();
-        private string loggedUser = "";
         noviZahtjev nZ;
         public static int narID;
+
         /// <summary>
         /// kreiramo metodu(dretvu) koja ce pratiti status novopristiglih narudzbi
         /// </summary>
         public frmPregledNarudzbi()
         {
             InitializeComponent();
-            ///_status = status;
-            if (loggedUser != null)
 
-                timer.Tick += new EventHandler(timer_Tick); // svaki put kad timer ticka poziva se event timer_Tick
+            timer.Tick += new EventHandler(timer_Tick); // svaki put kad timer ticka poziva se event timer_Tick
             timer.Interval = (3000) * (1);              // ticka svaku sekundu
             timer.Enabled = true;                       // omoguci timer
             timer.Start();
@@ -37,36 +35,33 @@ namespace FOR_Application
         }
 
         /// <summary>
-        /// metoda koja ce se pozivati tijekom Tick event timera
+        /// metoda koja ce se pozivati tijekom Tick event timera uz odgovarajuci ispis poruke
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         void timer_Tick(object sender, EventArgs e)
         {
-            nZ.pregledajNoveZahtjeve();
+            if (nZ.pregledajNoveZahtjeve() != 0)
+            {
+
+                MessageBox.Show("Imate nove narudzbe!");
+                nZ.updateNoviZahthevi();
+
+                //ponovo refreshamo listu narudbe posto smo dobili informaciju da je pristigla nova
+                // TODO: This line of code loads data into the 'pi2013FastOrderdbDataSet.Artikli' table. You can move, or remove it, as needed.
+                this.artikliTableAdapter.Fill(this.pi2013FastOrderdbDataSet.Artikli);
+                // TODO: This line of code loads data into the 'pi2013FastOrderdbDataSet.Stavke_narudzbe' table. You can move, or remove it, as needed.
+                this.stavke_narudzbeTableAdapter.Fill(this.pi2013FastOrderdbDataSet.Stavke_narudzbe);
+                // TODO: This line of code loads data into the 'pi2013FastOrderdbDataSet.Narudzba' table. You can move, or remove it, as needed.
+                this.narudzbaTableAdapter.Fill(this.pi2013FastOrderdbDataSet.Narudzba);
+
+            }
         }
 
-        /// <summary>
-        /// metoda kojom cemo postaviti tekst u statusnu traku
-        /// </summary>
-        /// <param name="text"></param>
-        public static void napisiNoviStatus(string text)
-        {
-            _status.Text = text;
-
-        }
-        /// <summary>
-        /// metoda kojom mijenjamo vrijednost listboxa  koji nam sluzi za prikaz novih zahtjeva 
-        /// klikom na pojedini zahtjev moci cemo vidjeti sve atribute neke narudzbe gosta
-        /// </summary>
-        /// <param name="value"></param>
-        public void izmjenaStanja(string value)
-        {
-            this.lstStatus.Text = "(" + value + ") zahtjeva";
-        }
 
         private void frmPregledNarudzbi_Load(object sender, EventArgs e)
         {
+            ///punimo table adapter s onim artiklima koji su naruceni od gosta
             // TODO: This line of code loads data into the 'pi2013FastOrderdbDataSet.Artikli' table. You can move, or remove it, as needed.
             this.artikliTableAdapter.Fill(this.pi2013FastOrderdbDataSet.Artikli);
             // TODO: This line of code loads data into the 'pi2013FastOrderdbDataSet.Stavke_narudzbe' table. You can move, or remove it, as needed.
